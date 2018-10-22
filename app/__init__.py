be225a6cc5121831cb52d7faf8c719d3c61473f3
Chenls <chenls1997@gmail.com>
2018-10-22 19:06:07 +0800

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from .base import MyResponse
 from .weixin import weixin
+import os 
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["TOKEN"] = "wechat"
+    app.config["TOKEN"] = os.environ.get("WECHAT_TOKEN")
+    app.config["APP_SECRET"] = os.environ.get("WECHAT_APP_SECRET")
     app.response_class = MyResponse
     app.register_blueprint(weixin)
-    # app.("TOKEN", "wechat")("APPSECRET", "2043c4d45968c088048d9782cb69a988");
     return app
