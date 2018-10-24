8dcde6fca7fbfb83a79d4008a167189420473ea5
Chenls <chenls1997@gmail.com>
2018-10-24 18:51:27 +0800

    ？？

--- startup.sh
+++ startup.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
 authbind gunicorn -c ./gunicorn.conf.py
-=======
-authbind gunicorn --bind 0.0.0.0 --reload wsgi:application --access-logfile='./gunicorn.access.log'
->>>>>>> 09431946c364fa104b0e463ba882889e4d4b4dd7
