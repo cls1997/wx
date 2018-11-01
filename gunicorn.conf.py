569e4920f901a3e740b39e10db2d9352b1b262ae
Chenls <chenls1997@gmail.com>
2018-11-01 12:24:00 +0800

    update loglevel debug

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,4 +4,4 @@ bind = "0.0.0.0:80"
 workers = 1
 errorlog = '{}/gunicorn.error.log'.format(sys.path[0])
 accesslog = '{}/gunicorn.error.log'.format(sys.path[0])
-#loglevel = 'debug'
+loglevel = 'debug'
