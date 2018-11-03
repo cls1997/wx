d825a4d8e014b1700eebb1c25efd9e5173fc3247
Chenls <chenls1997@gmail.com>
2018-11-03 11:25:18 +0800

    update :(

--- app/handlers/location.py
+++ app/handlers/location.py
@@ -5,19 +5,27 @@ def test(msg):
     return msg.msg_type == "location"
 
 
+# def respond(msg):
+#     import time
+#     import requests
+
+#     url = "http://api.map.baidu.com/timezone/v1?location={},{}".format(
+#         msg.location_x, msg.location_y)
+#     # make comma dont encode 
+#     params = {
+#         "coord_type": "wgs84ll",
+#         "timestamp": int(time.time()),
+#         "ak": "ivTL8OfP0G8gOeUYaMSP9SHfuXcxKfDX"
+#     }
+
+#     r = requests.get(url, params)
+
+#     return "text", {"Content": r.text}
 def respond(msg):
-    import time
-    import requests
+    from app.api.baidueagle import entity_create, entity_list, track_addpoint
 
-    url = "http://api.map.baidu.com/timezone/v1?location={},{}".format(
-        msg.location_x, msg.location_y)
-    # make comma dont encode 
-    params = {
-        "coord_type": "wgs84ll",
-        "timestamp": int(time.time()),
-        "ak": "ivTL8OfP0G8gOeUYaMSP9SHfuXcxKfDX"
-    }
+    wechat_user = msg.FromUserName
+    if entity_list(wechat_user)['status'] == 3003:
+        entity_create(wechat_user)
 
-    r = requests.get(url, params)
-
-    return "text", {"Content": r.text}
+    track_addpoint(wechat_user, msg.latitude, msg.longitude, msg.create_time)
